30d5e9e4b03938aa2bddb70423477037f11e1069
shijie09 <jiesj09@gmail.com>
2023-10-06 12:55:29 -0700

    update

--- assignmentOne/client.py
+++ assignmentOne/client.py
@@ -1,28 +1,40 @@
 import socket
 
-# Define server information
-server_ip = "50.123.106.98"  
-server_port = 5001       
+server_ip = "10.18.40.117"  
+server_port = 6767       
 
-# Get user input
 client_name = input("Enter your name: ")
 client_number = input("Enter an integer between 1 and 100: ")
 
-# Create a socket for the client
+# Create a socket for the client and connect to server
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# Connect to the server
 client_socket.connect((server_ip, server_port))
-
-# Prepare data to send
 data = "{},{}".format(client_name, client_number)
 
 # Send data to the server
 client_socket.send(data.encode())
+server_response = client_socket.recv(1024).decode()
 
 # Receive and print the server's response
-server_response = client_socket.recv(1024).decode()
-print("Server response: {}".format(server_response))
+try:
+    # Receive and print the server's response
+    server_name, server_number = server_response.split(',')
+    server_number = int(server_number)
 
-# Close the client socket
-client_socket.close()
+    client_number = int(client_number)
+    total_sum = client_number + server_number
+
+    print("Client Name: {}".format(client_name))
+    print("Server Name: {}".format(server_name))
+    print("Client Number: {}".format(client_number))
+    print("Server Number: {}".format(server_number))
+    print("Sum: {}".format(total_sum))
+    client_socket.close()
+
+except Exception as e:
+    print("Error: You should enter a number in range 0 to 100")
+    client_socket.close()
+
+
+
+
--- assignmentOne/server.py
+++ assignmentOne/server.py
@@ -1,29 +1,22 @@
 import socket
 import threading
 
-# Define server information
-server_ip = "192.168.68.52"
-server_port = 5001
+server_ip = "10.18.40.117"
+server_port = 6767
 
 # Function to handle client requests
 def handle_client(client_socket):
     try:
         # Receive data from the client
         data = client_socket.recv(1024).decode()
-
-        # Extract client's name and number
         client_name, client_number = data.split(',')
 
-        # Process the data and send a response
         server_number = 33
         server_response = "Server of Shijie,{}".format(server_number)
 
         if 1 <= int(client_number) <= 100:
-            # Calculate the sum
             client_number = int(client_number)
             sum_values = client_number + server_number
-
-            # Send the response back to the client
             client_socket.send(server_response.encode())
 
             print("Client Name: {}".format(client_name))
@@ -32,7 +25,6 @@ def handle_client(client_socket):
             print("Server Number: {}".format(server_number))
             print("Sum: {}".format(sum_values))
         else:
-            # Handle out-of-range input
             client_socket.send("Error: Integer out of range (1-100)".encode())
     except Exception as e:
         print("Error handling client:", e)
@@ -40,22 +32,17 @@ def handle_client(client_socket):
         # Close the client socket
         client_socket.close()
 
-# Create a socket for the server
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Bind the socket to the server address
 server_socket.bind((server_ip, server_port))
 
-# Listen for incoming connections
 server_socket.listen(5)  # Allow up to 5 connections at a time
 
 print("Server is listening on {}:{}".format(server_ip, server_port))
 
 while True:
-    # Accept a client connection
     client_socket, client_address = server_socket.accept()
     print("Accepted connection from {}:{}".format(client_address[0], client_address[1]))
 
-    # Create a new thread to handle the client
     client_thread = threading.Thread(target=handle_client, args=(client_socket,))
     client_thread.start()
